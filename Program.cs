7d52b7944171d83cb1eb3b42c3d5d061f9d54a27
MyGuy11 <35243044+MyGuy11@users.noreply.github.com>
2022-02-14 13:08:21 -0800

    Initial commit pt2
    Other initial commits, and adding the project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FriendAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing FriendAnalyzer");
+            Console.WriteLine("Analyzing FriendList....");
+            Console.WriteLine("Candidates found!");
+            Console.WriteLine("User: Pega");
+            Console.WriteLine("Reason: Mutual Server");
+            Console.WriteLine("Reason: Mutual Friends");
+            Console.WriteLine("Reason: Compatible");
+            Console.WriteLine("Send friend request(s)? (y/n)");
+            string answer = Console.In.ReadLine();
+            while (!(answer.Equals("y") || answer.Equals("Y") || answer.Equals("n") || answer.Equals("N")))
+            {
+                Console.WriteLine("I said (y/n), loner");
+                answer = Console.In.ReadLine();
+            }
+            if (answer.Equals("y") || answer.Equals("Y")) { Console.WriteLine("Friend request(s) sent"); }
+            if (answer.Equals("n") || answer.Equals("N")) { Console.WriteLine("Friend request(s) not sent"); }
+        }
+    }
+}
