d1f4684098300aa53b42e51435a6206bb946ff3a
MyGuy11 <35243044+MyGuy11@users.noreply.github.com>
2022-02-15 01:14:08 -0800

    Thread.Sleep addition
    Adding pauses to make the program flow better.

--- FriendAnalyzer.cs
+++ FriendAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Threading;
+using System;
 
 namespace Analyzer
 {
@@ -7,12 +8,21 @@ namespace Analyzer
         static void Main(string[] args)
         {
             Console.WriteLine("Initializing FriendAnalyzer");
-            Console.WriteLine("Analyzing FriendList....");
-            Console.WriteLine("Candidates found!");
+            Thread.Sleep(2000);
+            Console.Write("Analyzing FriendList");
+            for (int i = 0; i < 4; i++) // Replace with a while loop that waits for analysis task (Here's where async comes into play, Jasuv)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Done");
+            Thread.Sleep(500);
+            Console.WriteLine("Candidates found!"); 
             Console.WriteLine("User: Pega");
             Console.WriteLine("Reason: Mutual Server");
             Console.WriteLine("Reason: Mutual Friends");
             Console.WriteLine("Reason: Compatible");
+            Thread.Sleep(500);
             Console.WriteLine("Send friend request(s)? (y/n)");
             string answer = Console.In.ReadLine();
             while (!(answer.Equals("y") || answer.Equals("Y") || answer.Equals("n") || answer.Equals("N")))
