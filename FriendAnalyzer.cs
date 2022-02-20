79bc66b73391860772fff130c5c7d4dc2873a12d
Jasuv <ejasuv@gmail.com>
2022-02-20 11:30:28 -0800

    Jasuv is based
    I fixed it...

--- FriendAnalyzer.cs
+++ FriendAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System;
 
@@ -6,43 +7,103 @@ namespace Analyzer
 {
     class FriendAnalyzer
     {
+        string friendAnalyzerFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ".1FriendAnalyzer");
+        string peopleListTextPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ".1FriendAnalyzer\\PeopleList.txt");
+        public List<Person> peopleList = new List<Person>();
         static void Main(string[] args)
         {
+            FriendAnalyzer newAnalyzer = new FriendAnalyzer();
+            newAnalyzer.TheAnalyzer();
+        }
+        
+        void TheAnalyzer()
+        {
+            if (!Directory.Exists(friendAnalyzerFolderPath))  
+            {  
+                Directory.CreateDirectory(friendAnalyzerFolderPath);
+            } 
             Console.WriteLine("Initializing FriendAnalyzer");
             Thread.Sleep(2000);
             Console.Write("Analyzing FriendList");
-            for (int i = 0; i < 4; i++) // Replace with a while loop that waits for analysis task (Here's where async comes into play, Jasuv)
+            for (int i = 0; i < 4; i++)
             {
                 Console.Write(".");
                 Thread.Sleep(1000);
             }
-            Console.WriteLine("Done");
-            Thread.Sleep(500);
-            Console.WriteLine("Candidates found!");
-            Console.WriteLine("User: Pega");
-            Console.WriteLine("Reason: Mutual Server");
-            Console.WriteLine("Reason: Mutual Friends");
-            Console.WriteLine("Reason: Compatible");
-            Thread.Sleep(500);
-            Console.WriteLine("Send friend request(s)? (y/n)");
-            string answer = Console.In.ReadLine().ToLower();
-            FileStream file = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "eric.txt"), FileMode.Create);
-            using (StreamWriter writer = new StreamWriter(file))
+            
+            if (File.Exists(peopleListTextPath))
             {
-                writer.WriteLine("This is how u make a txt file");
-                writer.Close();
+                //write proper friend request stuff here
+
+                /*Console.WriteLine("\nSend friend request(s)? (y/n)");
+                string answer = Console.In.ReadLine().ToLower();
+                while (!(answer.Equals("y") || answer.Equals("n")))
+                {
+                    Console.WriteLine("I said (y/n), loner");
+                    answer = Console.In.ReadLine().ToLower();
+                }
+                if (answer.Equals("y")) { Console.WriteLine("Friend request(s) sent"); }
+                if (answer.Equals("n")) { Console.WriteLine("Friend request(s) not sent"); }*/
             }
-
-            file.Close();
-            file.Dispose();
-
-            while (!(answer.Equals("y") || answer.Equals("n")))
+            else
             {
-                Console.WriteLine("I said (y/n), loner");
-                answer = Console.In.ReadLine().ToLower();
+                Console.WriteLine("\nIt appears your people list is empty...");
+                Console.WriteLine("Please input at least 3 people into the list\n");
+                Thread.Sleep(500);
+                for(int i = 0; i < 3; i++)
+                {
+                    AddPeople();
+                }
+                using (StreamWriter personList = File.CreateText(peopleListTextPath))
+                {
+                    foreach(Person person in peopleList)
+                    {
+                        personList.WriteLine(person.name);
+                        foreach(string tag in person.tags)
+                        {
+                            personList.WriteLine(tag);
+                        }
+                    }
+                }
+                Console.Write("Creating new people list");
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("\n");
+                TheAnalyzer();
             }
-            if (answer.Equals("y")) { Console.WriteLine("Friend request(s) sent"); }
-            if (answer.Equals("n")) { Console.WriteLine("Friend request(s) not sent"); }
         }
+
+        void AddPeople()
+        {
+            Console.WriteLine("Name? ");
+            string inName = Console.In.ReadLine().ToLower();
+            Console.WriteLine("Input at least 1 tag for " + inName + ". Type stop to finish...");
+            List<string> inTags = new List<string>();
+            bool exit = false;
+            while(!exit)
+            {
+                string inTag = Console.In.ReadLine().ToLower();
+                if (inTag != "stop") {inTags.Add(inTag);}
+                else {exit = true;}
+            }
+            Console.WriteLine("Adding " + inName + " to people list...\n");
+            Person newPerson = new Person(inName, inTags);
+            peopleList.Add(newPerson);
+        }
+    }
+
+    public class Person
+    {
+        public string name;
+        public List<string> tags;
+
+        public Person(string inName, List<string> inTags)
+        {
+            name = inName;
+            tags = inTags;
+        }   
     }
 }
